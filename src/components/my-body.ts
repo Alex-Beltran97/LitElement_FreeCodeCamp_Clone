8d7032580187ffe17052f2bb0f8a9b29b456c12f
Alex-Beltran97 <pebeltranhe@gmail.com>
2023-08-07 00:00:45 -0500

    Landscape mode was adjusted only for mobile devices

--- src/components/my-body.ts
+++ src/components/my-body.ts
@@ -41,7 +41,7 @@ export class MyBody extends LitElement {
       cursor: pointer;
     }
 
-    @media screen and (max-width:768px), (orientation:landscape) {
+    @media screen and (max-width:768px) and (orientation:landscape) {
       :host {
         font-size: 1.1em;
         padding: 0 1em;
